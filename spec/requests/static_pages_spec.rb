246bec153d4f23de632bcd77530d72ca6e20fec7
lvs-taguchi <kenta.taguchi@livesense.co.jp>
2016-06-02 12:07:05 +0900

    5章まで完了

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Work App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Work App')
-    end
+    it { should have_content('Work App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
 
+  describe "Help page" do
+    before { visit help_path }
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
